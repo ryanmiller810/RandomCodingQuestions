efa92f4bdb6dc7c3dfa205c6c04fdc1a3fd7804a
ryanmiller810 <ryanmiller810@gmail.com>
2017-05-23 16:15:05 -0700

    Init

---
+++ ConsoleApp1/FileReader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class FileReader
+    {
+        public static void ReadFile(string fileName)
+        {
+            List<string> list = new List<string>();
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+                while((line = sr.ReadLine()) != null)
+                {
+                    list.Add(line);
+                    Console.WriteLine(line);
+                }
+
+            }
+        }
+
+        public static void ParseFile(string fileName)
+        {
+            string line;
+            string[] delimitedStrings;
+            //int position = 0;
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    delimitedStrings = line.Split('.');
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Reverser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Reverser
+    {
+        public static string ReverseStringWithoutLibrary(string s)
+        {
+            char[] array = new char[s.Length];
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                array[(s.Length - 1) - i] = s[i];
+            }
+            return new string(array);
+        }
+    }
+}
